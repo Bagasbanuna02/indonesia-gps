8bb0d317ff263f7947cde5d77601dfd8880b8a90
Bagasbanuna02 <bagasbanuna02@gmail.com>
2023-07-28 14:34:17 +0800

    ok

--- src/app/gambar/page.tsx
+++ src/app/gambar/page.tsx
@@ -9,7 +9,7 @@ export default function Gambar() {
     <Center>
    <Box>
    <Image
-        src="https://raw.githubusercontent.com/bipproduction/eagle-eye-project/16cbc58d5d0058f2045c996cf5eb27333f1cdb81/public/gps_2.svg?token=A5B2NEDSGGZNAG7UOA3XFQDEYNOLG"
+        src="/gps_2.svg"
         alt="GPS Indo"
         width={`${100+ "vw"}`}
         height={`${100+"vh"}`}
