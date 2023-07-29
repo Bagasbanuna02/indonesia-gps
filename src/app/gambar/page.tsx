44cee0de890fbd6b752d8d0be3b5a7ed0fe29b35
Bagasbanuna02 <bagasbanuna02@gmail.com>
2023-07-29 15:12:27 +0800

    ok

--- src/app/gambar/page.tsx
+++ src/app/gambar/page.tsx
@@ -13,31 +13,27 @@ import {
 
 import { AiOutlineArrowRight } from "react-icons/ai";
 import { BiSolidRightArrowAlt } from "react-icons/bi";
-import { useState, useEffect } from 'react';
-import { useInterval, useShallowEffect } from '@mantine/hooks';
+import { useState, useEffect } from "react";
+import { useInterval, useShallowEffect } from "@mantine/hooks";
 import _ from "lodash";
 
 export default function Gambar() {
-
   const [seconds, setSeconds] = useState(111);
-  const interval = useInterval(() => setSeconds((s) => _.random(100, 999)), 1000);
-
+  const interval = useInterval(
+    () => setSeconds((s) => _.random(100, 999)),
+    1000
+  );
 
   useShallowEffect(() => {
     interval.start();
     return interval.stop;
-  },[])
-
+  }, []);
 
   return (
     <>
-    <pre>
-
-    {JSON.stringify(seconds)}
-    </pre>
       <Box>
-        <Grid>
-          <Grid.Col span={"auto"}>
+        <Flex>
+          <Box w={"70%"}>
             <AspectRatio ratio={620 / 400} maw={1300} mx="auto" h={"100vh"}>
               <Center h={"100vh"} w={"100%"}>
                 <Image
@@ -48,64 +44,67 @@ export default function Gambar() {
                 />
               </Center>
             </AspectRatio>
-          </Grid.Col>
-          <Grid.Col span={"content"}>
-            <Flex
-              align={"start"}
-              justify={"center"}
-              h={"100vh"}
-              direction={"column"}
-              mr={100}
+          </Box>
+
+          <Flex
+            // bg={"blue"}
+            align={"center"}
+            justify={"center"}
+            h={"100vh"}
+            direction={"column"}
+            w={"30%"}
+            sx={{
+              fontSize: 9,
+            }}
+          >
+            <Box
+              my={10}
+              sx={{
+                color: "white",
+              }}
             >
-              <Box
-                my={10}
-                sx={{
-                  color: "white",
-                }}
-              >
-                <Text>DATA MINING VIEW DEVICE:</Text>
-                <Text>
-                  <BiSolidRightArrowAlt />
-                  ANDROID: 203.029.{`${seconds}`}
-                </Text>
-                <Text>
-                  <BiSolidRightArrowAlt />
-                  IOS: NULL
-                </Text>
-                <Text>
-                  <BiSolidRightArrowAlt />
-                  Huawei: NULL
-                </Text>
-              </Box>
+              <Text>DATA MINING VIEW DEVICE:</Text>
+              <Text>
+                <BiSolidRightArrowAlt />
+                ANDROID: 203.029.{`${seconds}`}
+              </Text>
+              <Text>
+                <BiSolidRightArrowAlt />
+                IOS: NULL
+              </Text>
+              <Text>
+                <BiSolidRightArrowAlt />
+                Huawei: NULL
+              </Text>
+            </Box>
 
-              <Box
-                my={10}
-                sx={{
-                  color: "white",
-                }}
-              >
-                <Text>AUDIANCE VALUE LOCKED:</Text>
-                <Text>
-                  <BiSolidRightArrowAlt />
-                  ANDROID: 161.938.032
-                </Text>
-              </Box>
+            <Box
+              my={10}
+              sx={{
+                color: "white",
+              }}
+            >
+              <Text>AUDIANCE VALUE LOCKED:</Text>
+              <Text>
+                <BiSolidRightArrowAlt />
+                ANDROID: 161.938.032
+              </Text>
+            </Box>
 
-              <Box
-                my={10}
-                sx={{
-                  color: "white",
-                }}
-              >
-                <Text>AUDIANCE VALUE FILTERED:</Text>
-                <Text>
-                  <BiSolidRightArrowAlt />
-                  ANDROID: 21.100.601
-                </Text>
-              </Box>
-            </Flex>
-          </Grid.Col>
-        </Grid>
+            <Box
+              my={10}
+              sx={{
+                color: "white",
+              }}
+            >
+              <Text>AUDIANCE VALUE FILTERED:</Text>
+              <Text>
+                <BiSolidRightArrowAlt />
+                ANDROID: 21.100.601
+              </Text>
+            </Box>
+          </Flex>
+        </Flex>
       </Box>
     </>
   );
